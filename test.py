67ba42cf148aa19d72bce98a4bc9395f59a823b3
battlecatsahead <48861210+battlecatsahead@users.noreply.github.com>
2022-10-16 14:31:19 -0400

    Add files via upload

---
+++ test.py
@@ -0,0 +1,31 @@
+import os
+import time
+import sys
+import random
+
+
+def diceroll(range):
+    print(random.randint(0,limit))
+
+print("how many sides?: ")
+limit = input()
+limit = int(limit)
+diceroll(limit)
+print("reroll?: y/n")
+limit = limit
+retry = input()
+str(retry)
+
+converter_string = str(retry)
+
+if retry == "y":
+    print("how many sides?: ")
+    limit = input()
+    limit = int(limit)
+    diceroll(limit)
+elif input == "n" :
+   exit()
+
+
+
+
